f7d67839e984ef99b6929ee9e88f2b5dea7f9f65
mark lucking <mark.lucking@gmail.com>
2020-10-08 09:17:06 +0200

    Initial Commit

---
+++ ChartPieAndGraphs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChartPieAndGraphs
+//
+//  Created by localadmin on 08.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
